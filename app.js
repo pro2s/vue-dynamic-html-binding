a1be61dba64dd83d745eeabc293aa77bec57dd62
Sergey Protasov <protasov.s.s@gmail.com>
2020-11-18 23:30:32 +0300

    Init

---
+++ app.js
@@ -0,0 +1,54 @@
+const app = Vue.createApp({
+    el: '#app',
+    data: () => ({
+        cards: [],
+        rows: [],
+        days: 7,
+    }),
+    methods: {
+        async add() {
+            try {
+                const result = await fetch('single.html')
+                const text = await result.text()
+                this.rows.push(text)
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        async addComponent() {
+            try {
+                const result = await fetch('single.html')
+                const text = await result.text()
+                this.cards.push(text)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+})
+
+app.component('raw-html', {
+    props: ["html"],
+    render: function (createElement) {
+        const render = Vue.compile(this.html)
+        return render(this.$parent)
+    },
+})
+
+app.component('card-component', {
+    props: ["html"],
+    data: () => ({
+        days: 1,
+    }),
+    methods: {
+        add() {
+            this.days++
+        }
+    },
+    render: function (context) {
+        const render = Vue.compile(this.html)
+        return render(context)
+    },
+})
+
+app.mount('#app')
